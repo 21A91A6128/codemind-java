6908f887fde8c6a023b6a573c042d3aec50060a9
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-09-17 13:13:08 +0530

    import java.util.Scanner; class cse{     public static void main(String args[]){         int v,k,l;         Scanner sc=new Scanner(System.in);         v=sc.nextInt();         l=sc.nextInt();         for(k=1;k<=l;k=k+2)         {             System.out.println(v+" x "+k+" = "+v*k);         }         sc.close();     } }

---
+++ Tables.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class cse{
+    public static void main(String args[]){
+        int v,k,l;
+        Scanner sc=new Scanner(System.in);
+        v=sc.nextInt();
+        l=sc.nextInt();
+        for(k=1;k<=l;k=k+2)
+        {
+            System.out.println(v+" x "+k+" = "+v*k);
+        }
+        sc.close();
+    }
+}
