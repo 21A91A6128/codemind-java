a39bfa4a311894bdd0e9fed215f5ef908ec3f015
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-09-23 10:16:31 +0530

    import java.util.*; class pat{ 	public static void main(String[] args) { 	    int r,a=0; 	    Scanner sc=new Scanner(System.in); 	    r=sc.nextInt(); 		for(int i=0;i<r;i++){ 		    for(int j=0;j<r;j++){ 		        if(i==j){ 		            System.out.print(0); 		        } 		        else{ 		            System.out.print("x"); 		        } 		    } 		    System.out.println(); 		} 	} }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,12 +1,17 @@
 import java.util.*;
 class pat{
 	public static void main(String[] args) {
-	    int r;
+	    int r,a=0;
 	    Scanner sc=new Scanner(System.in);
 	    r=sc.nextInt();
 		for(int i=0;i<r;i++){
-		    for(int j=r;j>=1;j--){
-		        System.out.print(j+" ");
+		    for(int j=0;j<r;j++){
+		        if(i==j){
+		            System.out.print(0);
+		        }
+		        else{
+		            System.out.print("x");
+		        }
 		    }
 		    System.out.println();
 		}
