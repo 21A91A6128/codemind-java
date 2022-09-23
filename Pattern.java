8bab6e4b39b8d47ed3d8c7f479a030041cc5461d
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-09-23 11:19:30 +0530

    import java.util.*; class pat{ 	public static void main(String[] args){ 	    int r; 	    Scanner sc=new Scanner(System.in); 	    r=sc.nextInt(); 	    int c=r; 		for(int i=1;i<=r;i++){ 		    for(int j=1;j<=r;j++){ 		           System.out.print(i+" "); 		        } 		    System.out.println(); 		} 	} }

---
+++ Pattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class pat{
+	public static void main(String[] args){
+	    int r;
+	    Scanner sc=new Scanner(System.in);
+	    r=sc.nextInt();
+	    int c=r;
+		for(int i=1;i<=r;i++){
+		    for(int j=1;j<=r;j++){
+		           System.out.print(i+" ");          
+		        }
+		    System.out.println();
+		}
+	}
+}
