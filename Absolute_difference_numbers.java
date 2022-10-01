6d17c8c069fbd832612d7978a23b383e8f1f8a1e
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-10-02 03:05:29 +0530

    import java.util.*; class adn{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int vk=sc.nextInt();         int r=(int)(Math.log10(vk));         int n=sc.nextInt();         int v=vk%((int)(Math.pow(10,n)));         int k=vk/((int)(Math.pow(10,r-n+1)));         System.out.print(Math.abs(v-k));     } }

---
+++ Absolute_difference_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class adn{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int vk=sc.nextInt();
+        int r=(int)(Math.log10(vk));
+        int n=sc.nextInt();
+        int v=vk%((int)(Math.pow(10,n)));
+        int k=vk/((int)(Math.pow(10,r-n+1)));
+        System.out.print(Math.abs(v-k));
+    }
+}
