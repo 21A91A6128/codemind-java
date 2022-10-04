a0b91dde98e6d9a5a95a30059b3dbe81580cc7bb
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-10-04 11:12:16 +0530

    import java.util.*; class foc{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         float v=sc.nextInt();         double k=(double)(v-32)/1.8;         System.out.format("%.2f",k);     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class foc{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float v=sc.nextInt();
+        double k=(double)(v-32)/1.8;
+        System.out.format("%.2f",k);
+    }
+}
