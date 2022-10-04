3369b09e683d014c74ccb441d9a3e6f3925d9e0d
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-10-04 11:13:31 +0530

    import java.util.*; class cof{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         float v=sc.nextInt();         double k=(double)(v*9/5)+32;         System.out.format("%.2f",k);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class cof{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float v=sc.nextInt();
+        double k=(double)(v*9/5)+32;
+        System.out.format("%.2f",k);
+    }
+}
