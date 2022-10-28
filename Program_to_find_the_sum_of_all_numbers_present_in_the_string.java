e176876cb5bff181823bbff08b7cfb6a8b777ba7
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-10-28 17:33:20 +0530

    import java.util.*; class sumnum{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String vk=sc.nextLine();         int sum=0;         for (int i=0;i<vk.length();i++)             if (Character.isDigit(vk.charAt(i)))                 sum+=vk.charAt(i)-'0';         System.out.print(sum);     } }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sumnum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String vk=sc.nextLine();
+        int sum=0;
+        for (int i=0;i<vk.length();i++)
+            if (Character.isDigit(vk.charAt(i)))
+                sum+=vk.charAt(i)-'0';
+        System.out.print(sum);
+    }
+}
