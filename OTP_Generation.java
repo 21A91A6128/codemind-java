6172e011b2687cda4a358d85747267fde46c4495
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2022-10-28 17:29:33 +0530

    import java.util.*; class otp{     public static void main(String args[]){         Scanner sc=new Scanner (System.in);         String s=sc.nextLine(),t="";         for (int i=0;i<s.length();i++){             int c=Integer.valueOf(s.charAt(i));             int d=Math.abs(48-c);             if (c%2!=0)                 t+=(d*d);         }         System.out.print(t);     } }

---
+++ OTP_Generation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class otp{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        String s=sc.nextLine(),t="";
+        for (int i=0;i<s.length();i++){
+            int c=Integer.valueOf(s.charAt(i));
+            int d=Math.abs(48-c);
+            if (c%2!=0)
+                t+=(d*d);
+        }
+        System.out.print(t);
+    }
+}
